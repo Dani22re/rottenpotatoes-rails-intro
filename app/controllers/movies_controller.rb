9c888d26a317a73ed3b359493c365e20f69c0b4d
Dr3368 Danielle Reich <d.reich2@columbia.edu>
2025-10-01 19:42:54 +0000

    Part 3: remember sort+filter via session; RESTful index; fix show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,45 +1,42 @@
 class MoviesController < ApplicationController
+  def index
+    @all_ratings = Movie.all_ratings
+
+    # redirect to RESTful URL if we have saved prefs but no params
+    if params[:ratings].blank? && params[:sort_by].blank?
+      if session[:ratings].present? || session[:sort_by].present?
+        return redirect_to movies_path(
+          ratings: (session[:ratings].presence && Hash[session[:ratings].product(['1'])]),
+          sort_by: session[:sort_by]
+        )
+      end
+    end
+
+    # which ratings to show
+    @ratings_to_show =
+      if params[:ratings].present?
+        params[:ratings].keys
+      elsif session[:ratings].present?
+        session[:ratings]
+      else
+        @all_ratings
+      end
+    @ratings_to_show = @all_ratings if @ratings_to_show.empty?
+
+    # sort column (whitelist)
+    requested_sort = params[:sort_by].presence || session[:sort_by]
+    @sort_by = %w[title release_date].include?(requested_sort) ? requested_sort : nil
+
+    # persist choices
+    session[:ratings] = @ratings_to_show
+    session[:sort_by] = @sort_by
+
+    # query
+    @movies = Movie.with_ratings(@ratings_to_show)
+    @movies = @movies.order(@sort_by) if @sort_by.present?
+  end
+
   def show
     @movie = Movie.find(params[:id])
-  end
-
-  def index
-    @all_ratings    = Movie.all_ratings
-    @ratings_to_show = params[:ratings]&.keys || @all_ratings
-    @movies         = Movie.with_ratings(@ratings_to_show)
-  end
-
-  def new
-    @movie = Movie.new
-  end
-
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
-
-  def edit
-    @movie = Movie.find(params[:id])
-  end
-
-  def update
-    @movie = Movie.find(params[:id])
-    @movie.update!(movie_params)   # Rails 8: use update!
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
-
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
-
-  private
-
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 end
